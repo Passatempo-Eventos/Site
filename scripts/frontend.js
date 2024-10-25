01134012e58f936216764960eaeba4dcd6682398
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-10-24 22:59:53 -0300

    feat: menu lateral abre e fecha ao clicar no icone do menu

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -1,9 +1,5 @@
-function openMenu() {
-    const navMenu = document.querySelector(".nav-menu")
-    if (navMenu.classList.contains("d-none")) {
-        navMenu.classList.remove("d-none")
-    }
-    else {
-        navMenu.classList.add("d-none")
-    }
+const openMenu = () => {
+    const sidebar = document.querySelector(".sidebar")
+
+    sidebar.classList.toggle("d-none")
 }
