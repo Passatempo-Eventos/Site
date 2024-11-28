07aa67876569b2327c9be574dd6ef22dba2d3ec0
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-11-28 15:55:42 -0300

    feat: criacao de token apos usuario ser autenticado (jwt)

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -6,7 +6,7 @@ const usuarioController = require("../controllers/usuarioController.js")
 // Rota para criar um usuário.
 router.post("/signup", usuarioController.criar)
  
-// Rota para verificar usuário.
-router.post("/signin", usuarioController.verificar)
+// Rota para autenticar usuário.
+router.post("/login", usuarioController.autenticar)
 
 module.exports = router
