a877ec607603ada322cecde9f7b3d70ed0e8cd81
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-12-04 12:39:09 -0300

    fix: separado renderizacao de eventos sociais e corporativos

--- backend/controllers/eventosController.js
+++ backend/controllers/eventosController.js
@@ -2,27 +2,28 @@ const Evento = require("../models/Evento.js")
 
 exports.mostrar = async (req, res) => {
     try {
-        const eventos = await Evento.find()
+        const eventosSociais = await Evento.find({ tipo: "social"})
+        const eventosCorporativos = await Evento.find({ tipo: "corporativo"})
         //res.status(200).json({ eventos })
-        res.render("eventos", { eventos })
+        res.render("eventos", { eventosSociais, eventosCorporativos })
     } catch (e) {
         res.status(500).json({ msg: "Erro ao buscar eventos." })
     }
 }
 
 exports.criar = async (req,res) => {
-    const { titulo, descricao, imagem } = req.body
-    const novoEvento = new Evento({ titulo, descricao, imagem })
+    const { tipo, titulo, descricao, imagem } = req.body
+    const novoEvento = new Evento({ tipo, titulo, descricao, imagem })
     await novoEvento.save()
     res.status(200).json({ msg: "Evento criado com sucesso!"})
 }
 
 exports.atualizar = async (req, res) => {
     const id = req.params.id
-    const { titulo, descricao, imagem } = req.body
+    const { tipo, titulo, descricao, imagem } = req.body
     try {
         const filtro = { _id: id }
-        const atualizar = { $set: { titulo, descricao, imagem }}
+        const atualizar = { $set: { tipo, titulo, descricao, imagem }}
         const resultado = await Evento.updateOne(filtro, atualizar)
         res.status(200).json({ msg: "Evento atualizado com sucesso!" })
     } catch(e) {
--- backend/models/Evento.js
+++ backend/models/Evento.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
 const eventoSchema = new mongoose.Schema({
+    tipo: { type: String, enum: ["social", "corporativo"], required: true },
     titulo: { type: String, required: true },
     descricao: { type: String, required: true },
     imagem: { type: String, required: true },
